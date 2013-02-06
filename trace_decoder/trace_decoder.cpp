38ac8c01b4ff417b91ff3be7f81d3879e1937e9d
Mathias Gottschlag <mgottschlag@gmail.com>
2013-02-06 13:48:55 +0100

    trace_decoder: Adapted the code to the new tracer format.

--- trace_decoder/trace_decoder.cpp
+++ trace_decoder/trace_decoder.cpp
@@ -5,29 +5,68 @@
 
 #include "vcdecoder.h"
 
-int main() {
+void decodeOldTracer(const std::string &line) {
     char buffer[1024];
+    size_t separator = line.find(": ");
+    if (separator == std::string::npos) {
+        return;
+    }
+    std::cout << line.substr(0, separator + 2);
+    std::istringstream parser(line.substr(separator + 2));
+    unsigned int address;
+    parser >> std::hex >> address;
+    std::string direction;
+    parser >> direction;
+    unsigned int value;
+    parser >> std::hex >> value;
+    vc_decode_register(address, buffer, sizeof(buffer));
+    std::cout << buffer;
+    std::cout << " " << direction << " ";
+    vc_decode_value(address, value, buffer, sizeof(buffer));
+    std::cout << buffer << std::endl;
+}
+
+void decode(const std::string &line, bool store) {
+    char buffer[1024];
+    std::string pc = line.substr(2, 8);
+    std::stringstream addressStr;
+    addressStr << std::hex << line.substr(11, 8);
+    unsigned int address;
+    addressStr >> address;
+    std::stringstream valueStr;
+    valueStr << std::hex << line.substr(19, 8);
+    unsigned int value;
+    valueStr >> value;
+    std::cout << "MMIO(";
+    if (store) {
+        std::cout << "W";
+    } else {
+        std::cout << "R";
+    }
+    std::cout << ", 4, 0x" << pc << "): ";
+    vc_decode_register(address, buffer, sizeof(buffer));
+    std::cout << buffer;
+    if (store) {
+        std::cout << " <= ";
+    } else {
+        std::cout << " => ";
+    }
+    vc_decode_value(address, value, buffer, sizeof(buffer));
+    std::cout << buffer << std::endl;
+}
+
+int main() {
     std::string line;
     while (!std::cin.eof()) {
         std::getline(std::cin, line);
-        size_t mmio = line.find("MMIO");
-        size_t separator = line.find(": ");
-        if (separator == std::string::npos || mmio == std::string::npos) {
-            continue;
+        if (line.find("MMIO") != std::string::npos) {
+            decodeOldTracer(line);
+        } else if (line.substr(0, 1) == "r") {
+            decode(line, false);
+        } else if (line.substr(0, 1) == "w") {
+            decode(line, true);
         }
-        std::cout << line.substr(0, separator + 2);
-        std::istringstream parser(line.substr(separator + 2));
-        unsigned int address;
-        parser >> std::hex >> address;
-        std::string direction;
-        parser >> direction;
-        unsigned int value;
-        parser >> std::hex >> value;
-        vc_decode_register(address, buffer, sizeof(buffer));
-        std::cout << buffer;
-        std::cout << " " << direction << " ";
-        vc_decode_value(address, value, buffer, sizeof(buffer));
-        std::cout << buffer << std::endl;
     }
     return 0;
 }
+
